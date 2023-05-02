e7f5fcf566542ff917aad053e970cc9f7f090271
ataman <atamanenkoalex90@gmail.com>
2023-05-02 22:40:21 +0300

    Add index page and change directory to base.html

--- university/university/urls.py
+++ university/university/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
+
 urlpatterns = [
+    path("", views.index, name="index"),
     path("admin/", admin.site.urls),
     path("student/", include("student.urls")),
     path("teacher/", include("teacher.urls")),
