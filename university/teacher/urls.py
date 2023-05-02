a6d8d3fb150e19ec3c98ae0c587a6011abed8ea1
ataman <atamanenkoalex90@gmail.com>
2023-05-02 17:04:46 +0300

    Add menu in templates

--- university/teacher/urls.py
+++ university/teacher/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.all_teachers, name="all_teachers"),
     path("create/", views.create_teacher, name="create_teacher"),
     path("<int:teacher_id>/", views.edit_teacher, name="edit_teacher"),
-    path("all-subjects", views.all_subjects, name="all_subjects"),
+    path("subject", views.all_subjects, name="all_subjects"),
     path("create-subject/", views.create_subject, name="create_subject"),
-    path("<int:subject_id>/", views.edit_subject, name="edit_subject"),
+    path("subject/<int:subject_id>/", views.edit_subject, name="edit_subject"),
 ]
