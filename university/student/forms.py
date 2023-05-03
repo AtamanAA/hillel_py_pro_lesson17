51e742c3c225ef1e6d341b87c37e168eaea2a1a8
ataman <atamanenkoalex90@gmail.com>
2023-05-04 01:05:45 +0300

    Add phone field to Student model and form

--- university/student/forms.py
+++ university/student/forms.py
@@ -1,4 +1,6 @@
 from django.forms import ModelForm, ModelMultipleChoiceField, CheckboxSelectMultiple
+from django.core.exceptions import ValidationError
+import phonenumbers
 
 from .models import Student
 from group.models import Group
@@ -11,6 +13,7 @@ class StudentForm(ModelForm):
             "first_name",
             "last_name",
             "age",
+            "phone",
             "group",
         ]
 
@@ -19,3 +22,13 @@ class StudentForm(ModelForm):
         widget=CheckboxSelectMultiple(),
         required=False,
     )
+
+    def clean_phone(self):
+        phone_raw = self.cleaned_data["phone"]
+        try:
+            phone = phonenumbers.parse(phone_raw, None)
+        except phonenumbers.phonenumberutil.NumberParseException:
+            raise ValidationError("Phone isn't valid!")
+        if not phonenumbers.is_valid_number(phone):
+            raise ValidationError("Phone isn't valid!")
+        return phonenumbers.format_number(phone, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
--- university/student/models.py
+++ university/student/models.py
@@ -6,6 +6,7 @@ class Student(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     age = models.PositiveIntegerField(validators=[MaxValueValidator(120)])
+    phone = models.CharField(max_length=20, null=True)
 
     def __repr__(self):
         return f"{self.first_name} {self.last_name}"
