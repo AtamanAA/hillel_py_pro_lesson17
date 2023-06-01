d170ee0e8b7e26783950181ff234af32ce1e6b94
ataman <atamanenkoalex90@gmail.com>
2023-06-01 17:18:02 +0300

    Update local DB settings

--- university/settings.py
+++ university/settings.py
@@ -119,8 +119,12 @@ else:
     # to simplify initial setup. Longer term it's recommended to use Postgres locally too.
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "postgres",
+            "USER": "postgres",
+            "PASSWORD": "docker",
+            "HOST": "localhost",
+            "PORT": "5400",
         }
     }
 
